bf32d64e7a00f33039cd1f9b8850bde541a31378
Mauricio Moreno <mmoren01@protonmail.com>
2024-03-11 19:22:21 -0700

    Installed MUI

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
-import "@/styles/globals.css";
+import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { Roboto } from 'next/font/google'
+import "@/styles/globals.css"
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const theme = createTheme({
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+})
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AppCacheProvider {...pageProps}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AppCacheProvider>
+  )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document"
+import {
+  DocumentHeadTags,
+  documentGetInitialProps,
+} from '@mui/material-nextjs/v13-pagesRouter'
 
-export default function Document() {
+export default function Document(props) {
   return (
     <Html lang="en">
       <Head />
+      <DocumentHeadTags {...props} />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
+}
+
+Document.getInitialProps = async (ctx) => {
+  const finalProps = await documentGetInitialProps(ctx)
+  return finalProps
 }
